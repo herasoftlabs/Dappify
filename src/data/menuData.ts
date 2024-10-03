637a476dfe1824c8a882833f20b1aa92c88a7551
dmrdvn <demirdoven.hh@gmail.com>
2024-10-03 20:34:22 +0300

    Updated Menu elements

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -6,7 +6,7 @@ export interface MenuItem {
   name: string;
   path?: string;
   subItems?: MenuItem[];
-  icon?: ReactNode; // Menü öğesi için ikon eklenebilir.
+  icon?: ReactNode;
 }
 
 export const menuData: MenuItem[] = [
@@ -36,27 +36,33 @@ export const menuData: MenuItem[] = [
 
 export const headerMenu = [
   {
+    id: 'dashboard',
+    name: 'Dashboard',
+    path: '/dashboard',
+   
+  },
+  {
     id: 'monitoring',
     name: 'Monitoring',
-    path: '/project-details',
+    path: '#',
    
   },
   {
     id: 'storage',
     name: 'Storage',
-    path: '/airdrop',
+    path: '#',
     
   },
   {
     id: 'domains',
     name: 'Domains',
-    path: '/domains',
+    path: '#',
    
   },
   {
     id: 'analytics',
     name: 'Analytics',
-    path: '/analytics',
+    path: '#',
   
   },
 ];
